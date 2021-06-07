4500806dadf6c4685dbe36cafc9a2ea7aaacc91b
Charlotte-zq <70183423+Charlotte-zq@users.noreply.github.com>
2021-06-07 22:35:35 +0800

    增加了一个用户类

---
+++ User.java
@@ -0,0 +1,16 @@
+
+public class User extends Object
+{
+	public String name;
+	private String pwd;
+	public int headpic;
+	public String getPwd()
+	{
+		return pwd;
+	}
+	public void setPwd(String pwd)
+	{
+		this.pwd = pwd;
+	}
+	
+}
